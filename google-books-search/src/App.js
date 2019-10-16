fba1cd2852b2b62d7ae08eb0bba3790d77968493
Ashlie Johnson <ashlie86.2003@yahoo.com>
2019-10-16 13:15:24 -0400

    added modal feature

--- google-books-search/src/App.js
+++ google-books-search/src/App.js
@@ -18,20 +18,31 @@ class App extends Component {
 
   render() {
     let books = this.state.books.map((book) => {
-      return(
+      return (
         <tr key={book.id}>
-              <td>{book.id}</td>
-              <td>{book.title}</td>
-              <td>{book.rating}</td>
-              <td>
-                <Button color="success" size="sm" className="mr-2">Edit</Button>
-                <Button color="danger" size="sm">Delete</Button>
-              </td>
-            </tr>
+          <td>{book.id}</td>
+          <td>{book.title}</td>
+          <td>{book.rating}</td>
+          <td>
+            <Button color="success" size="sm" className="mr-2">Edit</Button>
+            <Button color="danger" size="sm">Delete</Button>
+          </td>
+        </tr>
       )
     });
     return (
       <div className="App container">
+        <Button color="primary" onClick={this.toggleNewBookModal.bind(this)}>Add Book</Button>
+        <Modal isOpen={this.state.newBookModal} toggle={this.toggleNewBookModal.bind(this)} className={className}>
+          <ModalHeader toggle={toggle}>Modal title</ModalHeader>
+          <ModalBody>
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+        </ModalBody>
+          <ModalFooter>
+            <Button color="primary" onClick={toggle}>Do Something</Button>{' '}
+            <Button color="secondary" onClick={toggle}>Cancel</Button>
+          </ModalFooter>
+        </Modal>
         <Table>
 
           <thead>
@@ -47,9 +58,9 @@ class App extends Component {
             {books}
           </tbody>
         </Table>
-    </div >
-  );
-    
+      </div >
+    );
+
   }
 }
 export default App;
